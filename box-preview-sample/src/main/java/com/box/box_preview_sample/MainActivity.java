14b7286fb47dfc1037a839bfaaff69edab529a84
Don Cung <dcung@box.com>
2016-10-26 23:05:44 -0700

    Add logic for AppUser.

--- box-preview-sample/src/main/java/com/box/box_preview_sample/MainActivity.java
+++ box-preview-sample/src/main/java/com/box/box_preview_sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.box.box_preview_sample;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +11,13 @@ import android.widget.Toast;
 import com.box.androidsdk.browse.activities.BoxBrowseFileActivity;
 import com.box.androidsdk.content.BoxConfig;
 import com.box.androidsdk.content.BoxConstants;
+import com.box.androidsdk.content.BoxException;
 import com.box.androidsdk.content.auth.BoxAuthentication;
 import com.box.androidsdk.content.models.BoxFile;
 import com.box.androidsdk.content.models.BoxFolder;
 import com.box.androidsdk.content.models.BoxList;
 import com.box.androidsdk.content.models.BoxSession;
+import com.box.androidsdk.content.models.BoxUser;
 import com.box.androidsdk.preview.BoxPreviewActivity;
 
 /**
@@ -34,18 +37,48 @@ public class MainActivity extends AppCompatActivity implements BoxAuthentication
     private boolean mLoadedRoot;
     private static final String ROOT_IS_LOADED = "Bundle.Current_folder";
     private static final String PATH_TO_ROOT = "Bundle.Path_To_Root";
+    BoxAuthentication.AuthenticationRefreshProvider mRefreshProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BoxConfig.IS_LOG_ENABLED = true;
-        BoxConfig.CLIENT_ID = "186mddjxxv7vlyjxli70ur0tmdpkdgsi";
-        BoxConfig.CLIENT_SECRET = "psi8wEOuP5s9lDtfoKOUDvchjNcjxaFX";
+        final Context applicationContext = this;
+        mRefreshProvider = new BoxAuthentication.AuthenticationRefreshProvider() {
+            @Override
+            public BoxAuthentication.BoxAuthenticationInfo refreshAuthenticationInfo(BoxAuthentication.BoxAuthenticationInfo info) throws BoxException {
+                System.out.println("*** refreshAuthenticationInfo " + info);
+
+                return info;
+            }
+
+            @Override
+            public boolean launchAuthUi(String userId, BoxSession session) {
+                BoxAuthentication.BoxAuthenticationInfo info = new BoxAuthentication.BoxAuthenticationInfo();
+                info.setAccessToken("vEr4S4NeeOXuoSUruuxHVUr2ygIPcSpm");
+                info.setRefreshTime(System.currentTimeMillis());
+                String json = "{\"type\":\"user\",\"id\":\"406531322\",\"name\":\"enterprise\",\"login\":\"enterprisetestboxer+hack@gmail.com\",\"created_at\":\"2016-10-26T16:52:58-07:00\",\"modified_at\":\"2016-10-26T22:43:45-07:00\",\"language\":\"en\",\"timezone\":\"America\\/Los_Angeles\",\"space_amount\":10737418240,\"space_used\":7917800,\"max_upload_size\":262144000,\"status\":\"active\",\"job_title\":\"\",\"phone\":\"7703627258\",\"address\":\"\",\"avatar_url\":\"https:\\/\\/app.box.com\\/api\\/avatar\\/large\\/406531322\"}";
+                BoxUser user = new BoxUser();
+                user.createFromJson(json);
+                info.setUser(user);
+                BoxAuthentication.getInstance().onAuthenticated(info, applicationContext);
+                System.out.println("*** launchAuthUi " + userId + " " + session);
+                return true;
+            }
+        };
+        BoxAuthentication.getInstance().setRefreshProvider(mRefreshProvider);
+
+
+   //     BoxConfig.CLIENT_ID = "186mddjxxv7vlyjxli70ur0tmdpkdgsi";
+    //    BoxConfig.CLIENT_SECRET = "psi8wEOuP5s9lDtfoKOUDvchjNcjxaFX";
 
 
         // needs to match redirect uri in developer settings if set.
         BoxConfig.REDIRECT_URL = "https://app.box.com/static/sync_redirect.html";
+
+        // needs to match redirect uri in developer settings if set.
+  //      BoxConfig.REDIRECT_URL = "https://app.box.com/static/sync_redirect.html";
 
         if (savedInstanceState != null) {
             mPathToRoot = (BoxList<BoxFolder>) savedInstanceState.getSerializable(PATH_TO_ROOT);
@@ -142,6 +175,7 @@ public class MainActivity extends AppCompatActivity implements BoxAuthentication
         mSession.setSessionAuthListener(this);
         mSession.authenticate();
     }
+
 
     @Override
     public void onRefreshed(BoxAuthentication.BoxAuthenticationInfo info) {
